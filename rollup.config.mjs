1837e4def2d42cd5a561bad93cf0ab08cae1e67f
Rafael Nowrotek <mail@benignware.com>
2024-09-10 13:13:13 +0200

    Add bundle

---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+console.log('rollup.config.js');
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: 'index.mjs',
+  output: [
+    {
+      file: 'dist/themeEditorKit.mjs',
+      format: 'es'
+    },
+    {
+      file: 'dist/themeEditorKit.min.js',
+      format: 'iife',
+			name: 'ThemeEditorKit',
+      plugins: [terser()]
+    }
+  ]
+};
